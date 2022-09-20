a273e977dade208ddd53ddd0331ea9eb3c17db72
wlad9 <zav8b9@gmail.com>
2022-09-20 17:44:37 -0300

    Primeiro commit projeto BookstoreManager-Alura

---
+++ src/main/java/com/lixo/tbookstoremanager/TbookstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.lixo.tbookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbookstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lixo/tbookstoremanager/TbookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixo.tbookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
